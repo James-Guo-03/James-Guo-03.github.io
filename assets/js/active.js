68159422acb889a5e31e9adb08d335be5947bb35
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-07-16 19:15:32 +0800

    added active page listener

---
+++ assets/js/active.js
@@ -0,0 +1,19 @@
+if ("{{ page.title }}" === "Home" || "{{ page.category }}" === "Home") {
+    activeLink = document.getElementById("home");
+}
+if ("{{ page.title }}" === "CV" || "{{ page.category }}" === "CV") {
+    activeLink = document.getElementById("cv");
+}
+if ("{{ page.title }}" === "Notes" || "{{ page.category }}" === "Notes") {
+    activeLink = document.getElementById("notes");
+}
+if ("{{ page.title }}" === "Projects" || "{{ page.category }}" === "Projects") {
+    activeLink = document.getElementById("projects");
+}
+if ("{{ page.title }}" === "Teaching"  || "{{ page.category }}" === "Teaching") {
+    activeLink = document.getElementById("teaching");
+}
+if ("{{ page.title }}" === "Contact"  || "{{ page.category }}" === "Contact") {
+    activeLink = document.getElementById("contact");
+}
+activeLink.classList.add("active");
